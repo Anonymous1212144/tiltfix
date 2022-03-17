ead78aea7e0bf7e05cbfb27ad0d957e779153e0e
Anonymous1212144 <101783700+Anonymous1212144@users.noreply.github.com>
2022-03-17 00:16:30 -0400

    Add files via upload

---
+++ src/main/java/net/tilt/mixin/Knockback.java
@@ -0,0 +1,26 @@
+package net.tilt.mixin;
+
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class Knockback {
+	@Inject(at = @At("HEAD"), method = "renderWorld")
+	private void bobFix(float f, long j, MatrixStack l, CallbackInfo info) {
+		Object entity = ((GameRenderer)(Object)this).getClient().getCameraEntity();
+		if (entity instanceof LivingEntity) {
+			LivingEntity player = (LivingEntity) entity;
+			double x = player.getVelocity().x;
+			double z = player.getVelocity().z;
+			player.knockbackVelocity = (float) (MathHelper.atan2(-z, -x) * 57.2957763671875D - (double) player.getYaw());
+		}
+
+	}
+
+}
